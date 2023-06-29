b18d30f16757bcbb18f879a3600fec027b713cd7
joelachankeng <47440462+joelachankeng@users.noreply.github.com>
2023-06-29 07:11:03 -0400

    create route for games api

---
+++ api/games/index.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This is the route for the games api
+ * This route handles the REST operations for the games api controller
+ */
+
+require_once __DIR__ . "/../../controller/gamesApi.php";
+
+use Api\Games\Games;
+
+$games = new Games();
+
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(["error" => "Method not allowed"]);
+    exit;
+}
+
+
+// check if data is an error
+if ($games->data instanceof \Error) {
+    http_response_code($games->data->getCode());
+    echo $games->data->getMessage();
+    exit;
+}
+
+// check if data is false
+if ($games->data === false) {
+    http_response_code(500);
+    echo json_encode(["error" => "Internal Server Error"]);
+    exit;
+}
+
+// check if data is empty
+if (empty($games->data)) {
+    http_response_code(404);
+    echo json_encode(["error" => "No games found"]);
+    exit;
+}
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+echo json_encode($games->getPage($page));
